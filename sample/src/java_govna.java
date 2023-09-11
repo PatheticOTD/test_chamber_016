854e181cfff7d034076b856bd80b43bf285463da
Pathetic{OTD} <50541050+PatheticOTD@users.noreply.github.com>
2023-09-11 18:43:32 +0300

    11.09

--- sample/src/java_govna.java
+++ sample/src/java_govna.java
@@ -1,16 +1,19 @@
+import java.math.BigInteger;
+
 public class java_govna {
     
-    public static char charExpression(int a) {
-        char charValue = '\\';
-        int ans = (int) charValue;
-        ans += a;
-
-        return (char) ans;
-    }
+    public static BigInteger factorial(int value) {
+        BigInteger ans = BigInteger.valueOf(1);
+        for (int i = 1; i < value; i++){
+            BigInteger a = BigInteger.valueOf(i);
+            ans = ans.multiply(a);   
+        }
+        return ans; // your implementation here
+}
     public static void main(String[] args){
 
 
-        System.out.println(charExpression(32));
+        System.out.println(factorial(1));
 
     }
 }
