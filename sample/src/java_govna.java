1fbba8fc845e94897d2618f1f4d796b6bea41e96
Pathetic{OTD} <50541050+PatheticOTD@users.noreply.github.com>
2023-09-21 13:34:06 +0300

    sex update

--- sample/src/java_govna.java
+++ sample/src/java_govna.java
@@ -1,3 +1,5 @@
+import java.io.PrintStream;
+
 public class java_govna {
     public static void all_data_types(){
         boolean bool = true; //                  1 bit
@@ -15,16 +17,45 @@ public class java_govna {
         System.out.println(b+s+i+l+f+d);
         System.out.println(bool);
         System.out.println(S+c);
+    }
 
+    static class Empl{
+        String name;
+        String job_name;
+        String email;
+        String phone;
+        double salary;
+        int age;
+        public Empl(String _name, String _job_name, String _email, String _phone, double _salary, int _age){
+            name = _name;
+            job_name = _job_name;
+            email = _email;
+            phone = _phone;
+            salary = _salary;
+            age = _age;
 
+        }
+        
+        public void info(){
+            System.out.println("name:"+ name +"\njob_name: "+ job_name +"\nemail: "+email+"\nnumber: "+phone+"\nsalary: "+salary+"\nage:"+ age+"\n");
+        }
 
     }
 
     public static void main(String[] args){
-        System.out.println("sosaka");
-        all_data_types();
-        //System.out.println(1);
-        //System.out.println(1);
+
+        Empl[] dude = new Empl[4];
+
+
+        dude[0] = new Empl("Petrov Petr", "Designer", "petpetr@mailbox.com", "891998765", 35000, 28);
+        dude[1] = new Empl("Sidorov Sidor", "Programmer", "sidsid@mailbox.com", "890544332", 40000, 302);
+        dude[2] = new Empl("Smirnov Sergey", "Manager", "smsmir@mailbox.com", "897712345", 42000, 42);
+        dude[3] = new Empl("Kuznetsov Kirill", "Accountant", "kirkuz@mailbox.com", "896633221", 38000, 31);
+        for(int i = 0; i<4; i++){
+            if(dude[i].age >= 40){
+                dude[i].info();
+            }
+        }
 
 
     }
