4a58f8b5056328f655a4819cb85f911d9b1f0304
Pathetic{OTD} <50541050+PatheticOTD@users.noreply.github.com>
2023-10-16 17:46:13 +0300

    no java )

--- Leetcode_extreeme_algorithms.py
+++ Leetcode_extreeme_algorithms.py
@@ -1,11 +1,28 @@
-strs = ["flower","flow","fliwht"]
-strans = ''
 
-z = list(zip(*strs))
-ans = map(lambda x: x[0] if x.count(x[0]) == len(x) else '', z)
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-for i in ans:
-    if i=='':break
-    strans+=i
 
-print(strans)
+def addTwoNumbers(self, l1, l2):
+    o = 0
+    ans = ListNode()
+    
+    while (l1.val != None) and (l2.val != None):
+        if (l1.val + l2.val + o)%10 == 0:
+            ans.val = l1.val + l2.val + o
+            o = 0
+        else:
+            ans.val = (l1.val + l2.val + o)%10
+            o = 1
+        ans.next
+        l1.next
+        l2.next
+    return ans
+
+a = ListNode()
+a.val = 1
+a.next
+a = a.next
+print(a.value)
